001c5814e7e6e55133a2f250cd6d4d4320404f26
ScottW <webster.j.scott@gmail.com>
2025-03-27 11:37:19 -0400

    Adding basic elements

---
+++ script.js
@@ -0,0 +1,29 @@
+const modal = document.getElementById("modal");
+let currentSlide = 0;
+const images = ["img1.jpg", "img2.jpg", "img3.jpg"]; // Replace with real paths
+
+function openModal() {
+  modal.classList.remove("hidden");
+  updateCarousel();
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % images.length;
+  updateCarousel();
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + images.length) % images.length;
+  updateCarousel();
+}
+
+function updateCarousel() {
+  document.getElementById("carousel-img").src = images[currentSlide];
+  document.querySelectorAll(".dot").forEach((dot, idx) => {
+    dot.classList.toggle("active", idx === currentSlide);
+  });
+}
