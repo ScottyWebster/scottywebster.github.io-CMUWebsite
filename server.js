9b38a108c2fb66a8add681b9467dcd4ca1206c4d
ScottW <webster.j.scott@gmail.com>
2026-02-23 23:50:41 -0500

    why won't u work

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 // Middleware to parse JSON and serve your HTML/CSS/JS files
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "dist")));
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 // Global chat history for the session
 let chatHistory = [
   {
